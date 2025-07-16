6c2bcf493a57a996d5c27129e09a88480a53b358
Exy5 <georgweiss90@gmail.com>
2025-07-16 21:10:55 +0200

    changed page.tsx in dynamic loading page

--- src/app/guides/[raid]/[boss]/page.tsx
+++ src/app/guides/[raid]/[boss]/page.tsx
@@ -1,6 +1,3 @@
-// app/guides/[raid]/[boss]/page.tsx
-
-
 import fs from 'fs/promises';
 import path from 'path';
 import { notFound } from 'next/navigation';
@@ -8,20 +5,26 @@ import { compileMDX } from 'next-mdx-remote/rsc';
 import { components } from '@/app/components/mdxComponents';
 import GuideContent from '@/app/components/Guides/GuideContent/GuideContent';
 
-export default async function BossGuidePage({ params }: { params: { raid: string; boss: string } } | { params: Promise<{ raid: string; boss: string }> }) {
-  const resolvedParams = params instanceof Promise ? await params : params;
-  const { raid, boss } = resolvedParams;
+interface PageProps {
+  params: Promise<{ raid: string; boss: string }>;
+}
+
+export default async function BossGuidePage({ params }: PageProps) {
+  const { raid, boss } = await params;
   const filePath = path.join(process.cwd(), 'src', 'content', raid, `${boss}.mdx`);
+  
   try {
     await fs.access(filePath);
   } catch {
     return notFound();
   }
+  
   const source = await fs.readFile(filePath, 'utf8');
   const { content } = await compileMDX({
     source,
     components,
   });
+  
   return (
     <GuideContent content={content} />
   );
