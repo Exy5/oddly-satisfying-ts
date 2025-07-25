2cd794a69644b22748ea06ff3687c7ae3404dd03
Exy5 <georgweiss90@gmail.com>
2025-07-25 18:04:13 +0200

    added elegon; fixed styling; removed test section

--- src/app/components/Guides/BossButton/BossButton.tsx
+++ src/app/components/Guides/BossButton/BossButton.tsx
@@ -2,12 +2,20 @@
 import React from "react";
 import Link from "next/link";
 import { bossButton, bossButtonImage, bossButtonText } from "./BossButton.css";
+import Image from 'next/image'
 
 export default function BossButton({ href, name, image }: { href: string; name: string; image?: string }) {
   return (
     <Link href={href} className={bossButton}>
       {image && (
-        <img src={image} alt="" className={bossButtonImage} aria-hidden />
+        <Image 
+          src={image} 
+          alt="" 
+          className={bossButtonImage} 
+          aria-hidden 
+          width={56}
+          height={56}
+        />
       )}
       <span className={bossButtonText}>{name}</span>
     </Link>
